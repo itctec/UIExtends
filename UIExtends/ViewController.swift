0e39357b4e48a13649ac21f68a40657b48960297
Itc <itctec@126.com>
2019-01-07 15:19:53 +0800

    UISearchBar测试

--- UIExtends/ViewController.swift
+++ UIExtends/ViewController.swift
@@ -8,13 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UISearchBarDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //UISearchBar
+        let searchBar=UISearchBar(frame: CGRect(x: 10, y: 30, width: 350, height: 30))
+        searchBar.tintColor=UIColor.red
+        searchBar.placeholder="请输入搜索内容"
+        searchBar.showsScopeBar=true
+        searchBar.showsCancelButton=true
+        searchBar.showsBookmarkButton=true
+        searchBar.showsSearchResultsButton=true
+        searchBar.scopeButtonTitles=["One","Two","Three"]
+        searchBar.delegate=self
+        self.view.addSubview(searchBar)
+        
+        
     }
 
-
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        print("取消")
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        print("搜索")
+    }
+    
+    
 }
 
